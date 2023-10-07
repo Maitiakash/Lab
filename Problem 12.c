ed769b6cf0dbe0a7d7e891ae4ab5df6a74e0eee0
Maitiakash <145957772+Maitiakash@users.noreply.github.com>
2023-10-07 18:30:39 +0530

    Add files via upload

---
+++ Problem 12.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+
+int main() {
+    int age;
+
+    // Prompt the user to enter their age
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    // Check if the age is greater than or equal to 18
+    if (age >= 18) {
+        printf("You are eligible to vote.\n");
+    } else {
+        printf("You are not eligible to vote.\n");
+    }
+
+    return 0;
+}
