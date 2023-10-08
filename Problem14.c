d00d8d3c7568afe19c43ada178ece23f1185e49d
Maitiakash <145957772+Maitiakash@users.noreply.github.com>
2023-10-08 22:28:48 +0530

    Add files via upload

---
+++ Problem14.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Size of float: %lu bytes\n", sizeof(float));
+    printf("Size of double: %lu bytes\n", sizeof(double));
+    printf("Size of long double: %lu bytes\n", sizeof(long double));
+    
+    return 0;
+}
